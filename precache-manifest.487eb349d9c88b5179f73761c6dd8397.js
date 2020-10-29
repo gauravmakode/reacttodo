96f8e1647aff220768825338a28640e3d428e41d
gourav makode <gouravmmakode002@gmail.com>
2020-10-29 21:43:03 +0530

    Updates

---
+++ precache-manifest.487eb349d9c88b5179f73761c6dd8397.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4a53d5034e75a784fff642291206892",
+    "url": "/reacttodo/index.html"
+  },
+  {
+    "revision": "4c0aaa6f492d73cc2dc6",
+    "url": "/reacttodo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d13b8139f3f0813d1c68",
+    "url": "/reacttodo/static/js/2.d55e4e83.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reacttodo/static/js/2.d55e4e83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c0aaa6f492d73cc2dc6",
+    "url": "/reacttodo/static/js/main.05bb94de.chunk.js"
+  },
+  {
+    "revision": "44566308d8877de9601f",
+    "url": "/reacttodo/static/js/runtime-main.94dd12f3.js"
+  }
+]);
